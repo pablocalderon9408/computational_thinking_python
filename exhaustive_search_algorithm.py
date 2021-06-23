70a54843d034a4ca0372d0edf637f8c0896da9e4
pablocalderon9408 <pablocalderon9408@gmail.com>
2021-06-23 11:33:09 -0500

    Hice el minimo producto viable

--- exhaustive_search_algorithm.py
+++ exhaustive_search_algorithm.py
@@ -1,11 +1,18 @@
 def exhaustive_search_algorithm():
     """ Calculate the square root to a number given by the user"""
-
+    answer = 0
     try:
-        number = int(input("Please enter a number to check it has an exact square root"))
+        number = int(input("Please enter a number to check if it has an exact square root: " + "\n"))
     except:
         pass
 
+    while answer ** 2 < number:
+        answer += 1
+
+    if answer ** 2 == number:
+        print("The number has an exact square root and it is: {answer}".format(answer=answer))
+    else:
+        print("The number {number} has not an exact square root.".format(number=number))
     
 if __name__ == "__main__":
     exhaustive_search_algorithm()
