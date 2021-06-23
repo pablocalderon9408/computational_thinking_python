0b04fb68fcab02e006d066ac62aeb6bb1e2b5925
pablocalderon9408 <pablocalderon9408@gmail.com>
2021-06-23 10:50:22 -0500

    I started with the logic of the program

---
+++ exhaustive_search_algorithm.py
@@ -0,0 +1,11 @@
+def exhaustive_search_algorithm():
+    """ Calculate the square root to a number given by the user"""
+
+    try:
+        number = int(input("Please enter a number to check it has an exact square root"))
+    except:
+        pass
+
+    
+if __name__ == "__main__":
+    exhaustive_search_algorithm()
